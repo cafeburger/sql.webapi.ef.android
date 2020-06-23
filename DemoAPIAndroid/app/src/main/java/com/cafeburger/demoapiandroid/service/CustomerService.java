f6055ae82fa89fa02eca940a448fd002d77cf395
Sam Liu <sam.liu@mail.searching-service.com>
2020-06-23 08:05:47 +0800

    add detailactivity

--- DemoAPIAndroid/app/src/main/java/com/cafeburger/demoapiandroid/service/CustomerService.java
+++ DemoAPIAndroid/app/src/main/java/com/cafeburger/demoapiandroid/service/CustomerService.java
@@ -7,11 +7,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.cafeburger.demoapiandroid.model.Customer;
 import com.cafeburger.demoapiandroid.utility.CallbackHandler;
 import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CustomerService {
     private final static String TAG = "CustomerService";
@@ -37,6 +41,26 @@ public class CustomerService {
         });
         queue.add(stringRequest);
 
+    }
+
+    public void updateCustomer(Customer customer, final CallbackHandler<Boolean> handler){
+        String url = "https://demoapi20200621194452.azurewebsites.net/api/customer/updatecustomer";
+        try {
+            JsonObjectRequest jsonRequest = new JsonObjectRequest(url, new JSONObject(new Gson().toJson(customer)), new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    handler.onComplete(true);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    handler.onError(error.getMessage());
+                }
+            });
+            queue.add(jsonRequest);
+        } catch (JSONException e) {
+            handler.onError(e.getMessage());
+        }
     }
 
 }
